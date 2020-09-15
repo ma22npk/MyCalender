9697c74e2a30d4519c596cd8811f9d32b3da8e5f
Kota Matsumoto <mac@mba.local>
2020-09-15 22:46:30 +0900

    年月を二桁表示

--- script.js
+++ script.js
@@ -64,6 +64,16 @@ console.clear()
   }
 
   function createCalendar() {
+    const tbody = document.querySelector('tbody');
+    //tbodyの最初の要素がある限りtbodyの最初の要素を削除する
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+    //年月をHTMLに描画する
+    //padStart関数で月が一桁の時は二桁で表示(padStart関数は文字列にしか使えない！)
+    const title = `${year}/${String(month + 1).padStart(2, '0')}`;
+    document.getElementById('title').textContent = title;
+
     const dates = [
       ...getCalendarHead(),
       ...getCalendarBody(),
